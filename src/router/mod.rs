2659457fd14f1e571253506d99a5d2f1a382bbc5
Rik <rikdevries253@hotmail.nl>
2018-06-28 15:25:58 +0200

    Adding routes if not present

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -26,55 +26,38 @@ impl Paths {
         if route.len() > 0 {
             if let Some(c) = route[0].chars().next() {
                 if c == ':' {
-                    self.add_wildcard_route(route[0]);
-                    let i = self.wildcard.len() - 1;
-                    self.wildcard[i].add_route(&route[1..]);
+                    // self.add_wildcard_route(route[0]);
+                    // let i = self.wildcard.len() - 1;
+                    // self.wildcard[i].add_route(&route[1..]);
+                    Paths::add_route_to_vec(route, &mut self.wildcard);
                 }else {
-                    self.add_sub_route(route);
+                    Paths::add_route_to_vec(route, &mut self.sub);
                 }
             }
         }
     }
 
-    fn add_wildcard_route(&mut self, name: &str) {
-        self.wildcard.push(Paths {
-            name: String::from(&name[1..]),
+    fn add_route_to_vec(route: &[&str], store: &mut Vec<Paths>) {   
+        //Search for existing route.
+        for i in store.iter_mut() {
+            if i.name == route[0] {
+                i.add_route(&route[1..]);
+                return;
+            }
+        }
+        //Create new route if there is not one already.
+        store.push(Paths {
+            name: String::from(route[0]),
             function: None,
             sub: Vec::new(),
             wildcard: Vec::new()
         });
+        let i = store.len() - 1;
+        let item = &mut store[i];
+        item.add_route(&route[1..]);
     }
 
-    fn add_sub_route(&mut self, route: &[&str]) {
-        let exists;
-        {
-            let current = self.find_sub(route[0]);
-            exists = match current {
-                Some(_item) => true,
-                None => false,
-            }
-        }
-        let mut item;
-        if exists {
-            for i in self.sub.iter_mut() {
-                if i.name == route[0] {
-                    item = i;
-                    break;
-                }
-            }
-        }else{
-            self.sub.push(Paths {
-                name: String::from(route[0]),
-                function: None,
-                sub: Vec::new(),
-                wildcard: Vec::new()
-            });
-            let i = self.sub.len() - 1;
-            item = &mut self.sub[i];
-        }
-        // item.add_route(&route[1..]);
-    }
-
+    /// Split a given route str at the '/' into a vector of the different parts 
     fn route_vec(route: &str) -> Vec<&str> {
         let mut list = Vec::new();
         let mut start = 0;
