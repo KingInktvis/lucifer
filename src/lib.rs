81b6403c785af2c8fefa3a45f41140030bbc5800
Rik de Vries <rikdevries253@hotmail.nl>
2018-07-25 11:58:56 +0200

    Changed to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,10 @@
-use std::net::TcpListener;
-use http::*;
 
 mod http;
 mod router;
 mod handler;
 mod middleware;
 
-fn main() {
-    let mut server = Server::new();
-    let mut routes = RouteHandler::new();
-    let mut middleware = middleware::MiddlewareStore::new();
-    middleware.add(Box::new(MW{}));
-    routes.add_route(Method::GET, "/", root);
-    server.listen("127.0.0.1:8000", routes, middleware);
-}
-
-fn root(_req: Request, _args: Args) -> Response {
-    let mut res = Response::new();
-    res.send_message(" <!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-
-</body>
-</html> ");
-    res
-}
-
-struct MW {}
-impl middleware::Middleware for MW {
-    fn call(&self, req: Request, args: Args, handle: &mut middleware::MiddlewareHandle) -> Response {
-        handle.next(req, args)
-    }
-}
-
-
+use std::net::TcpListener;
 use router::*;
 use handler::Manager;
 use middleware::*;
