4c52c4c1eabf364f45e330e40848d8f1c0fa0abd
Rik de Vries <rikdevries253@hotmail.nl>
2018-09-05 14:15:16 +0200

    Connection handling overhaul WIP

--- src/handler.rs
+++ src/handler.rs
@@ -8,79 +8,15 @@ use std::io::prelude::*;
 use std::sync::Arc;
 use middleware::*;
 
-pub struct Manager {
-    amount: usize,
-    workers: Vec<thread::JoinHandle<()>>,
-    tx: Vec<mpsc::Sender<TcpStream>>,
-    share: Arc<RouteHandler>,
-    middleware: Arc<MiddlewareStore>,
-    next_worker: usize
+pub struct Conductor {
 }
 
 #[allow(dead_code)]
-impl Manager {
-    pub fn new() -> Manager {
-        Manager {
-            amount: 1,
-            workers: Vec::new(),
-            tx: Vec::new(),
-            share: Arc::new(RouteHandler::new()),
-            middleware: Arc::new(MiddlewareStore::new()),
-            next_worker: 0
+impl Conductor {
+    pub fn new() -> Conductor {
+        Conductor {
+            
         }
-    }
-
-    pub fn set_thread_count(&mut self, amount: u32) {
-        if amount > 0 {
-            self.amount = amount as usize;
-        }
-    }
-
-    pub fn boot(&mut self, router: RouteHandler, middleware: MiddlewareStore) {
-        self.share = Arc::new(router);
-        self.middleware = Arc::new(middleware);
-
-        for _ in  0..self.amount {
-            let (tx, rx) = mpsc::channel();
-            let route_access = self.share.clone();
-            let middleware_store = self.middleware.clone();
-
-            let handle = thread::spawn(move || {
-                for mess in rx {
-                    Manager::handle_stream(mess, &route_access, &middleware_store);
-                }
-            });
-            self.workers.push(handle);
-            self.tx.push(tx);
-        }
-    }
-
-    pub fn pass_stream(&mut self, stream: TcpStream) {
-        if self.next_worker >= self.amount {
-            self.next_worker = 0;
-        }
-        match self.tx.get(self.next_worker) {
-            Some(t) => t.send(stream).unwrap(),
-            None => {}
-        }
-        self.next_worker += 1;
-    }
-
-    fn handle_stream(mut stream: TcpStream, router: &Arc<RouteHandler>, middleware: &Arc<MiddlewareStore>) {
-        let mut buffer = [0; 512];
-        stream.read(&mut buffer).unwrap();
-        let req = Request::new(&mut buffer);
-        let res;
-        if let Some(val) = req {
-            res = Manager::middleware_route_call(val, router, middleware);
-        }else {
-            let mut tmp = Response::new();
-            tmp.set_status(404);
-            res = tmp;
-        }
-
-        stream.write(&res.to_bytes()[..]).unwrap();
-        stream.flush().unwrap();
     }
 
     fn middleware_route_call(req: Request, router: &Arc<RouteHandler>, middleware: &Arc<MiddlewareStore>) -> Response {
--- src/lib.rs
+++ src/lib.rs
@@ -6,39 +6,77 @@
 pub mod http;
 pub mod router;
 pub mod middleware;
-mod handler;
 
-use std::net::TcpListener;
-use router::*;
-use handler::Manager;
-use middleware::*;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+//use router::*;
+//use middleware::*;
+//use std::io;
 
 #[allow(dead_code)]
 pub struct Server {
-    manager: Manager
+    thread_count: u32,
+    connections: Vec<Option<TcpStream>>,
+    threads: Vec<thread::JoinHandle<()>>
 }
 
 #[allow(dead_code)]
 impl Server {
     pub fn new() -> Server {
         Server {
-            manager: Manager::new()
+            thread_count: 2,
+            connections: Vec::new(),
+            threads: Vec::new()
         }
     }
 
-    pub fn set_thread_count(&mut self, count: u32) {
-        self.manager.set_thread_count(count);
+    fn boot_threads(&mut self) {
+        for i in 0..self.thread_count {
+
+        }
     }
 
-    pub fn listen(&mut self, address: &str, routes: RouteHandler, middleware: MiddlewareStore) {
+    pub fn listen(&mut self, address: &str) {
+        self.boot_threads();
         let listener = TcpListener::bind(address).unwrap();
-        self.manager.boot(routes, middleware);
         for stream in listener.incoming() {
             match stream {
-                Ok(stream) => self.manager.pass_stream(stream),
+                Ok(stream) => self.add_stream(stream),
                 Err(_) => print!("Error while unwrapping stream")
+            }
+            self.check_connections();
+        }
+    }
+
+    fn add_stream(&mut self, stream: TcpStream) {
+        for conn in self.connections.iter_mut() {
+            match *conn {
+                Some(_) => {},
+                None => {
+                    *conn = Some(stream);
+                    return
+                }
+            }
+        }
+        self.connections.push(Some(stream));
+    }
+
+    fn check_connections(&mut self) {
+        for option in self.connections.iter_mut() {
+            match option {
+                Some(conn) =>{
+                    let mut buf = [0; 8];
+                    let len = conn.peek(&mut buf);
+                    match len {
+                        Ok(l) => {
+//                            print!("{}\n", l);
+//                            print!("{}", String::from_utf8_lossy(&buf[0..l]));
+                        },
+                        Err(_) => {}
+                    }
+                },
+                None => {}
             }
         }
     }
 }
-
